3b8823988a3403ea9c8bee7dfa413a1ed3bd3b57
Eugene Nunoo <khemikal2016@gmail.com>
2023-06-08 18:12:40 +0100

    reg route"

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//routes
+app.use('/api/users',require('./routes/userRoutes'))
+
 app.listen(port, () => {
   console.log(`SERVER ACTIVE ON PORT ${port}`);
 });

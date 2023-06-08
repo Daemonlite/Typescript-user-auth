1b35fe461a3a1fc8c076041ccea8d94f58e69049
Eugene Nunoo <khemikal2016@gmail.com>
2023-06-08 18:27:15 +0100

    reg route"

--- src/handlers/userHandler.ts
+++ src/handlers/userHandler.ts
@@ -127,10 +127,10 @@ const register = async (req: Request, res: Response): Promise<void> => {
 
   try {
     await user.save();
-    res.status(201).json({
-      user,
-      token,
-    });
+    res.status(200).json({
+        ...user.toObject(),
+        token,
+      });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Failed to create user" });
